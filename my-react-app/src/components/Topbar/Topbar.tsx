44a9bd71c01ef38070b87a3aca9753f69ad9d82d
herkam3 <1078244@hr.nl>
2025-10-27 19:22:11 +0100

    Use public logo via /trophy.svg for the Vite asset rules

--- my-react-app/src/components/Topbar/Topbar.tsx
+++ my-react-app/src/components/Topbar/Topbar.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import UserMenu from "../UserMenu.tsx";
-import logo from "../../../public/trophy.svg";
 
 const TopBar: React.FC = () => {
 	return (
 		<nav className="topbar" role="navigation">
 			<div className="logo">
 				<Link to="/Home" aria-label="Home">
-					<img src={logo} alt="Logo" />
+					<img src="/trophy.svg" alt="Logo" />
 				</Link>
 			</div>
 
