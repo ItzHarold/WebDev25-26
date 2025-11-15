76b0cc7a143b934633db9c7cb692d121f794320e
Sidim9 <124693825+Sidim9@users.noreply.github.com>
2025-11-15 14:56:15 +0100

    11 make userfavourites model for backend (#30)
    * Add UserFavourite model (no migrations yet)
    
    * namespace added for UserFavourite
    
    ---------
    
    Co-authored-by: sidi <sidimo2001@gmail.com>
    Co-authored-by: Jurwin <76262883+JurDev@users.noreply.github.com>

---
+++ Backend/Models/UserFavourites/UserFavourite.cs
@@ -0,0 +1,8 @@
+namespace Backend.Models.UserFavourite;
+
+public class UserFavourite
+{
+    public int Id { get; set; }
+    public int UserId { get; set; }
+    public int EventId { get; set; }
+}
