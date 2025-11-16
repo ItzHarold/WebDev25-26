2c1717e8e7f50f3f0fdad3fa0ae3224efd7e0dfa
ItzHarold <haroldponte2003@gmail.com>
2025-11-16 22:13:54 +0100

    Add navigation properties to EventTeam model
    Introduced Event and Team navigation properties to the EventTeam class to support entity relationships and improve data access.

--- Backend/Models/EventTeam/EventTeam.cs
+++ Backend/Models/EventTeam/EventTeam.cs
@@ -4,6 +4,7 @@ public class EventTeam
 {
     public int Id { get; set; }
     public int EventId { get; set; }
+    public Event Event { get; set; } = null!;
     public int TeamId { get; set; }
-    
+    public Team Team { get; set; } = null!;
 }
