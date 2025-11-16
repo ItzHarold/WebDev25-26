4efe3aa897014507dbc668f13de9974de7bb1643
ItzHarold <haroldponte2003@gmail.com>
2025-11-16 22:08:27 +0100

    Add navigation properties to UserFavourite model
    Introduced User and Event navigation properties to the UserFavourite entity to enable easier access to related data and improve ORM relationships.

--- Backend/Models/UserFavourite/UserFavourite.cs
+++ Backend/Models/UserFavourite/UserFavourite.cs
@@ -4,5 +4,7 @@ public class UserFavourite
 {
     public int Id { get; set; }
     public int UserId { get; set; }
+    public User User { get; set; } = null!;
     public int EventId { get; set; }
+    public Event Event { get; set; } = null!;
 }
