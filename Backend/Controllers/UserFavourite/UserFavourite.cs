bf239197181b76b190315115e0b55bf1dd9904d7
JurDev <jurwinevertsz@live.nl>
2025-11-18 14:15:07 +0100

    Template UserFavourite

---
+++ Backend/Controllers/UserFavourite/UserFavourite.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Backend.Services;
+using Backend.Models;
+
+namespace Backend.Controllers.UserFavourite
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserFavourite : Controller
+    {
+        private readonly ILogger<UserFavourite> _logger;
+
+        public UserFavourite(ILogger<UserFavourite> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
