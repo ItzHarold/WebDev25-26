8b6bab7e1cd28c5ac195d0537e00cd54b7af2135
sidi <sidimo2001@gmail.com>
2025-12-03 16:48:22 +0100

    Event, EventTeam controllers omgezet naar DTO structuur en mapping verbeterd

--- Backend/Models/Event/Event.cs
+++ Backend/Models/Event/Event.cs
@@ -13,3 +13,26 @@ public class Event
     public ICollection<EventTeam> EventTeams { get; set; } = new List<EventTeam>();
     public ICollection<UserFavourite> FavouritedByUsers { get; set; } = new List<UserFavourite>();
 }
+
+public class EventRequest
+{
+    public string Title { get; set; } = null!;
+    public string? Location { get; set; }
+    public DateTime Date { get; set; }
+    public string? Description { get; set; }
+    public string? Detail { get; set; }
+    public string Status { get; set; } = null!;
+    public string? ImageUrl { get; set; }
+}  
+
+public class EventResponse
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public string? Location { get; set; }
+    public DateTime Date { get; set; }
+    public string? Description { get; set; }
+    public string? Detail { get; set; }
+    public string Status { get; set; } = null!;
+    public string? ImageUrl { get; set; }
+}
