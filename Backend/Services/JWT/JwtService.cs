8501f721b4f8b8d228dbe2ffe1cb9eb341c44b6b
JurDev <jurwinevertsz@live.nl>
2025-12-04 14:15:08 +0100

    Add Authenticate method stub to JwtService
    Introduced a commented-out stub for an asynchronous Authenticate method in JwtService, outlining the initial steps for user authentication using email and password. Also added missing Microsoft.EntityFrameworkCore import and renamed the constructor parameter for consistency.

--- Backend/Services/JWT/JwtService.cs
+++ Backend/Services/JWT/JwtService.cs
@@ -1,6 +1,7 @@
 using Backend.Data;
 using Backend.Models;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Services;
 
@@ -9,12 +10,27 @@ public class JwtService
     private readonly AppDbContext _dbContext;
     private readonly IConfiguration _configuration;
 
-    public JwtService(AppDbContext dbContext, IConfiguration configuration)
+    public JwtService(AppDbContext context, IConfiguration configuration)
     {
-        _dbContext = dbContext;
+        _dbContext = context;
         _configuration = configuration;
 
     }
+
+    /*public async Task<LoginResponse?> Authenticate(LoginRequest request)
+    {
+        if (string.IsNullOrWhiteSpace(request.Email) || string.IsNullOrWhiteSpace(request.Password))
+            return null;
+        var User = await _dbContext.Users.FirstOrDefaultAsync(x => x.Email == request.Email);
+        if (User is null || !PasswordService.Verify(User.Password, request.Password))
+            return null;
+        var issuer = _configuration["JwtConfig:Issuer"];
+        var audience = _configuration["JwtConfig:Audience"];
+        var key = _configuration["JwtConfig:Key"];
+        var tokenValidityMins = _configuration.GetValue<int>("JwtConfig:TokenValidityMins");
+        how
+
+    }*/
 
 
 
