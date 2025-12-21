1fc429769732d2a0e2d9c1b56999b048895bc39e
ItzHarold <1054428@hr.nl>
2025-12-21 01:37:12 +0100

    Implemented dropdown for all the pages at the top
    I implemented a component for the ui that allows the user to see all their respective pages depending on their role

--- frontend/src/shared/ui/AppLayout.tsx
+++ frontend/src/shared/ui/AppLayout.tsx
@@ -2,6 +2,9 @@ import { Outlet, NavLink, useNavigate } from "react-router-dom";
 import { useAuth } from "../../features/auth/AuthProvider";
 import "./AppLayout.css";
 import "../../pages/Login/LoginPage.css";
+import UserMenu from "./UserMenu";
+
+
 
 export default function AppLayout() { 
   const { logout, user } = useAuth();
@@ -18,17 +21,11 @@ export default function AppLayout() {
         <nav className="topbar_nav" aria-label="Main navigation">
           <a className="topbar_brand" href="/">WebDev25Logo</a>
           <ul className="topbar_links">
-            <li> <NavLink to="/">Home</NavLink> </li>
             <li className="topbar__spacer" />
-            <li> <NavLink to="/dashboard">Dashboard</NavLink> </li>
-            <li className="topbar__spacer" />
-            <li> <NavLink to="/about">About</NavLink> </li>
-            <li className="topbar__spacer" />
-            <li> <button type="button" className="topbar_logout" onClick={handleLogout}>Logout</button></li>
+            <li><UserMenu /></li>
           </ul>
         </nav>
       </header>
-
       <main className="main" id="main"><Outlet /></main>
 
     </>
